a4c79bcfd623568cc6a79568562d85df4e6547d5
Md. Fazlul Karim <md.fazlul.karim@g.bracu.ac.bd>
2025-02-07 00:04:24 +0600

    Client.py

--- Client.py
+++ Client.py
@@ -5,25 +5,25 @@ def start_client():
     host = '127.0.0.1'  # Server's IP address
     port = 12345        # Server's port number
 
-    # Create a socket object
+    
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Connect to the server
+    
     client_socket.connect((host, port))
     print(f"Connected to server at {host}:{port}")
 
     while True:
-        # Send a message to the server
+        
         message = input("Client: ")
         client_socket.send(message.encode())
 
-        # Receive a response from the server
+        
         data = client_socket.recv(1024).decode()
         if not data:
             break
         print(f"Server: {data}")
 
-    # Close the connection
+    
     client_socket.close()
 
 if __name__ == '__main__':
